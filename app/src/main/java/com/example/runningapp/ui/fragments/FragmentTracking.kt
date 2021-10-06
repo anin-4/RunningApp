3b3ac1e40481fa68ac0360f13153e6ee2c10f23a
anindya <anindyaray44@gamil.com>
2021-10-06 12:59:20 +0530

    set up mapview

--- app/src/main/java/com/example/runningapp/ui/fragments/FragmentTracking.kt
+++ app/src/main/java/com/example/runningapp/ui/fragments/FragmentTracking.kt
@@ -6,13 +6,58 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.runningapp.R
+import com.example.runningapp.databinding.FragmentTrackingBinding
+import com.google.android.gms.maps.GoogleMap
 
 class FragmentTracking : Fragment() {
+    private lateinit var binding:FragmentTrackingBinding
+    private var map:GoogleMap?= null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_tracking, container, false)
+    ): View {
+
+        binding= FragmentTrackingBinding.inflate(inflater,container,false)
+        binding.mapView?.onCreate(savedInstanceState)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.mapView.getMapAsync {
+            map=it
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView?.onSaveInstanceState(outState)
+    }
 }
